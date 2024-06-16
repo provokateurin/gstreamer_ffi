a91ae9436166280f9272c7470b1aee6f375a7a47
provokateurin <kate@provokateurin.de>
2024-06-16 08:39:10 +0200

    Initial commit

---
+++ bin/hello_world.dart
@@ -0,0 +1,45 @@
+import 'dart:ffi';
+
+import 'package:ffi/ffi.dart';
+import 'package:gstreamer_ffi/gstreamer_ffi.dart';
+
+/// Implementation of https://gstreamer.freedesktop.org/documentation/tutorials/basic/hello-world.html?gi-language=c
+void main() {
+  final dylib = DynamicLibrary.open('/usr/lib/libgstreamer-1.0.so.0');
+
+  final gst = GStreamer(dylib);
+
+  gst.gst_init();
+
+  final pipeline = gst.gst_parse_launch(
+    'playbin uri=https://gstreamer.freedesktop.org/data/media/sintel_trailer-480p.webm',
+  );
+
+  gst.gst_element_set_state(pipeline, GstState.GST_STATE_PLAYING);
+
+  final bus = gst.gst_element_get_bus(pipeline);
+  final msg = gst.gst_bus_timed_pop_filtered(
+    bus,
+    GST_CLOCK_TIME_NONE,
+    GstMessageType.GST_MESSAGE_ERROR | GstMessageType.GST_MESSAGE_EOS,
+  );
+
+  if (msg.ref.type == GstMessageType.GST_MESSAGE_ERROR) {
+    final err = gst.gst_message_parse_error(msg);
+
+    String? error_message;
+    if (err.gerror.ref.message.address != nullptr.address) {
+      error_message = err.gerror.ref.message.cast<Utf8>().toDartString();
+    }
+
+    malloc.free(err.gerror);
+    malloc.free(err.debug);
+
+    throw Exception(error_message);
+  }
+
+  // TODO: Unref msg, gst_message_unref missing due to https://github.com/dart-lang/native/issues/459
+  gst.gst_object_unref(bus);
+  gst.gst_element_set_state(pipeline, GstState.GST_STATE_NULL);
+  gst.gst_object_unref(pipeline);
+}
---
+++ lib/gstreamer_ffi.dart
@@ -0,0 +1,82 @@
+import 'dart:ffi';
+
+import 'package:ffi/ffi.dart';
+
+import 'src/generated.dart' as generated;
+
+export 'src/generated.dart' show GstState, GST_CLOCK_TIME_NONE, GstMessageType;
+
+class GStreamer {
+  GStreamer(DynamicLibrary dylib) : _gst = generated.GStreamer(dylib);
+
+  final generated.GStreamer _gst;
+
+  void gst_init() {
+    // TODO: Allow passing args
+    final _argc = malloc<Int>()..value = 0;
+    final _argv = malloc<Char>()..value = 0;
+
+    _gst.gst_init(_argc, Pointer.fromAddress(_argv.address));
+
+    malloc.free(_argc);
+    malloc.free(_argv);
+  }
+
+  Pointer<generated.GstElement> gst_parse_launch(
+    String pipeline_description,
+  ) {
+    final _pipeline_description = pipeline_description.toNativeUtf8().cast<Char>();
+    final _error = calloc<generated.GError>();
+
+    final _element = _gst.gst_parse_launch(_pipeline_description, Pointer.fromAddress(_error.address));
+
+    malloc.free(_pipeline_description);
+
+    if (_error.ref.message.address != nullptr.address) {
+      malloc.free(_error);
+
+      throw Exception(_error.ref.message.cast<Utf8>().toDartString());
+    }
+
+    malloc.free(_error);
+
+    return _element;
+  }
+
+  void gst_object_unref(Pointer object) {
+    _gst.gst_object_unref(object as generated.gpointer);
+  }
+
+  int gst_element_set_state(Pointer<generated.GstElement> element, int state) {
+    return _gst.gst_element_set_state(element, state);
+  }
+
+  Pointer<generated.GstBus> gst_element_get_bus(Pointer<generated.GstElement> element) {
+    return _gst.gst_element_get_bus(element);
+  }
+
+  Pointer<generated.GstMessage> gst_bus_timed_pop_filtered(Pointer<generated.GstBus> bus, int timeout, int types) {
+    return _gst.gst_bus_timed_pop_filtered(bus, timeout, types);
+  }
+
+  ({
+    Pointer<generated.GError> gerror,
+    Pointer<Char> debug,
+  }) gst_message_parse_error(
+    Pointer<generated.GstMessage> message,
+  ) {
+    final _gerror = calloc<generated.GError>();
+    final _debug = calloc<Char>();
+
+    _gst.gst_message_parse_error(
+      message,
+      Pointer.fromAddress(_gerror.address),
+      Pointer.fromAddress(_debug.address),
+    );
+
+    return (
+      gerror: _gerror,
+      debug: _debug,
+    );
+  }
+}
